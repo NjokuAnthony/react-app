fac196545770866a3359610279072c88b81bcc30
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2024-01-30 20:13:17 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../src/components/hero/Hero'
+import Navbar from './components/navbar/Navbar';
 
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Hero />
     </div>
   );
